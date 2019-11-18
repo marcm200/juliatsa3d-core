5670432bd4f4ddff94c8ca20249c64110543c20f
marcm200 <52798815+marcm200@users.noreply.github.com>
2019-11-18 16:54:07 +0100

    Add files via upload

---
+++ _basilica_makin.bat
@@ -0,0 +1,21 @@
+@echo off
+if not exist juliatsa3dcore_d.exe goto error:
+del _in.raw
+juliatsa3Dcore_d func=makin cmd=period len=8 c=-1,0,0 range=2 
+copy _3d.raw _in.raw /Y
+juliatsa3Dcore_d func=makin cmd=period len=9 c=-1,0,0 range=2 
+copy _3d.raw _in.raw /Y
+juliatsa3Dcore_d func=makin cmd=period len=10 c=-1,0,0 range=2 
+goto end:
+
+:error
+echo.
+echo Please copy the desired binary (win32, win64) with (opt) or without (noopt) compiler optimizations
+echo into the current directory and rename it to juliatsacore3d_d.exe
+echo.
+echo.
+
+goto end:
+
+:end
+pause
---
+++ _external_viewer.bat
@@ -0,0 +1,37 @@
+@echo off
+if not exist cube-viewer.exe goto error:
+if not exist juliatsa3dcore_d.exe goto error2:
+del _in.raw
+rem compute from scratch and save as bitcube
+juliatsa3Dcore_d func=makin cmd=calc len=8 c=-1,0,0 range=2 twdb=0
+rem showing interior cells as pure yellow
+cube-viewer file=bc1.ccb cmd=show observer=3 transp=250,250,250,255,255,255
+rem using computed data
+copy _3d.raw _in.raw /Y
+rem compute one refinement level higher and color periodic cycles
+juliatsa3Dcore_d func=makin cmd=period len=9 c=-1,0,0 range=2 twdb=0
+rem show immediate basins and attraction basins
+cube-viewer file=bc1per.ccb cmd=show observer=3 transp=250,250,250,255,255,255
+echo.
+echo.
+echo Note the different vieweing position (termed 3) as compared to the standard internal viewer (termed 1).
+echo.
+goto end:
+
+:error
+echo.
+echo Please copy cube-viewer.exe into the current directory and start again.
+echo.
+echo.
+goto end:
+
+:error2
+echo.
+echo Please copy the desired binary (win32, win64) with (opt) or without (noopt) compiler optimizations
+echo into the current directory and rename it to juliatsacore3d_d.exe
+echo.
+echo.
+goto end:
+
+:end
+pause
