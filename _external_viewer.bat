b0aaebba5b981792e9b00bbbde61508b0744ff72
marcm200 <52798815+marcm200@users.noreply.github.com>
2020-01-21 13:26:44 +0100

    Update _external_viewer.bat

--- _external_viewer.bat
+++ _external_viewer.bat
@@ -3,13 +3,13 @@ if not exist cube-viewer.exe goto error:
 if not exist juliatsa3dcore_d.exe goto error2:
 del _in.raw
 rem compute from scratch and save as bitcube
-juliatsa3Dcore_d func=makin cmd=calc len=8 c=-1,0,0 range=2 twdb=0
+juliatsa3Dcore_d func=makin cmd=calc len=8 c=-1,0,0 range=2 twdb=0 revcg=4
 rem showing interior cells as pure yellow
 cube-viewer file=bc1.ccb cmd=show observer=3 transp=250,250,250,255,255,255
 rem using computed data
 copy _3d.raw _in.raw /Y
 rem compute one refinement level higher and color periodic cycles
-juliatsa3Dcore_d func=makin cmd=period len=9 c=-1,0,0 range=2 twdb=0
+juliatsa3Dcore_d func=makin cmd=period len=9 c=-1,0,0 range=2 twdb=0 revcg=4
 rem show immediate basins and attraction basins
 cube-viewer file=bc1per.ccb cmd=show observer=3 transp=250,250,250,255,255,255
 echo.
